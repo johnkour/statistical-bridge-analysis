977345f6fba6d8a18c3df9204887961dfddff441
John Kouretas <64531567+johnkour@users.noreply.github.com>
2020-06-07 04:04:12 +0300

    Add files via upload
    Fixxing typos.

--- prob_model.m
+++ prob_model.m
@@ -41,7 +41,7 @@ ECDF.Color = '#EDB120';
 
 y = linspace(10, 40, 1000);
 
-%% =======================GUMBEL DISTRIBUTION==============================
+%% ================GENERALIZED EXTREME VALUE DISTRIBUTION==================
 
 pd = fitdist(wind, 'GeneralizedExtremeValue');
 
@@ -50,7 +50,7 @@ f(1,:) = pdf(pd, y);
 figure(1)
 hold on;
 plot(y, f(1,:), 'Color', [0.4660 0.6740 0.1880]);
-legend('Histogram', 'Gumbel');
+legend('Histogram', 'Generalized Extreme Value');
 hold off;
 
 F(1,:) = cdf(pd, y);
@@ -58,14 +58,14 @@ F(1,:) = cdf(pd, y);
 figure(2)
 hold on;
 plot(y, F(1,:), 'Color', [0.4660 0.6740 0.1880]);
-legend('Empirical', 'Gumbel');
+legend('Empirical', 'Generalized Extreme Value');
 hold off;
 
 figure(3)
-subplot(3, 3, 1); qqplot(wind, pd); title('Gumbel');
+subplot(3, 3, 1); qqplot(wind, pd); title('Generalized Extreme Value');
 
 figure(4)
-subplot(2, 3, 1); probplot(pd, wind); title('Gumbel');
+subplot(2, 3, 1); probplot(pd, wind); title('Generalized Extreme Value');
 
 
 %% =======================EXPONENTIAL DISTRIBUTION=========================
@@ -77,7 +77,7 @@ f(2,:) = exppdf(y, param);
 figure(1)
 hold on;
 plot(y, f(2,:), 'y');
-legend('Histogram', 'Gumbel', 'Exponential');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential');
 hold off;
 
 F(2,:) = expcdf(y, mean_w);
@@ -85,7 +85,7 @@ F(2,:) = expcdf(y, mean_w);
 figure(2)
 hold on;
 plot(y, F(2,:), 'y');
-legend('Empirical', 'Gumbel', 'Exponential');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential');
 hold off;
 
 pd = fitdist(wind, 'Exponential');
@@ -104,7 +104,8 @@ f(3,:) = pdf(pd,y);
 figure(1)
 hold on;
 plot(y, f(3,:), 'g');
-legend('Histogram', 'Gumbel', 'Exponential', 'Lognormal');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal');
 hold off;
 
 F(3,:) = logncdf(y, params(1), params(2));
@@ -112,7 +113,8 @@ F(3,:) = logncdf(y, params(1), params(2));
 figure(2)
 hold on;
 plot(y, F(3,:), 'g');
-legend('Empirical', 'Gumbel', 'Exponential', 'Lognormal');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal');
 hold off;
 
 figure(3)
@@ -127,7 +129,8 @@ f(4,:) = pdf(pd, y);
 figure(1)
 hold on;
 plot(y, f(4,:), 'm');
-legend('Histogram', 'Gumbel', 'Exponential', 'Lognormal', 'Normal');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal');
 hold off;
 
 F(4,:) = cdf(pd, y);
@@ -135,7 +138,8 @@ F(4,:) = cdf(pd, y);
 figure(2)
 hold on;
 plot(y, F(4,:), 'm');
-legend('Empirical', 'Gumbel', 'Exponential', 'Lognormal', 'Normal');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal');
 hold off;
 
 figure(3)
@@ -153,8 +157,8 @@ f(5,:) = wblpdf(y, params(1), params(2));
 figure(1)
 hold on;
 plot(y, f(5,:), 'Color', [0.3010 0.7450 0.9330]);
-legend('Histogram', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                 'Weibull');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull');
 hold off;
 
 F(5,:) = wblcdf(y, params(1), params(2));
@@ -162,8 +166,8 @@ F(5,:) = wblcdf(y, params(1), params(2));
 figure(2)
 hold on;
 plot(y, F(5,:), 'Color', [0.3010 0.7450 0.9330]);
-legend('Empirical', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential', ... 
+                'Lognormal', 'Normal', 'Weibull');
 hold off;
 
 pd = fitdist(wind, 'Weibull');
@@ -191,8 +195,8 @@ f(6,:) = gampdf(y, params(1), params(2));
 figure(1)
 hold on;
 plot(y, f(6,:), 'k');
-legend('Histogram', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma');
 hold off;
 
 F(6,:) = gamcdf(y, params(1), params(2));
@@ -200,8 +204,8 @@ F(6,:) = gamcdf(y, params(1), params(2));
 figure(2)
 hold on;
 plot(y, F(6,:), 'k');
-legend('Empirical', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma');
 hold off;
 
 pd = fitdist(wind, 'Gamma');
@@ -221,8 +225,9 @@ f(7,:) = pdf(pd, y);
 figure(1)
 hold on;
 plot(y, f(7,:), 'Color', [0.6350 0.0780 0.1840]);
-legend('Histogram', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma', 'ExtremeValue');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma', ...
+                'Extreme Value');
 hold off;
 
 F(7,:) = cdf(pd, y);
@@ -230,8 +235,9 @@ F(7,:) = cdf(pd, y);
 figure(2)
 hold on;
 plot(y, F(7,:), 'Color', [0.6350 0.0780 0.1840]);
-legend('Empirical', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma', 'Extreme Value');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma', ...
+                'Extreme Value');
 hold off;
 
 figure(3)
@@ -257,8 +263,9 @@ f(8,:) = pdf(pd, y);
 figure(1)
 hold on;
 plot(y, f(8,:), 'c');
-legend('Histogram', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma', 'ExtremeValue', 'Half Normal');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma', ...
+                'Extreme Value', 'Half Normal');
 hold off;
 
 F(8,:) = cdf(pd, y);
@@ -266,8 +273,9 @@ F(8,:) = cdf(pd, y);
 figure(2)
 hold on;
 plot(y, F(8,:), 'c');
-legend('Empirical', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma', 'ExtremeValue', 'Half Normal');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma', ...
+                'Extreme Value', 'Half Normal');
 hold off;
 
 figure(3)
@@ -282,9 +290,9 @@ f(9,:) = pdf(pd, y);
 figure(1)
 hold on;
 plot(y, f(9,:), 'r');
-legend('Histogram', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma', 'ExtremeValue', 'HalfNormal', ...
-                        'Nakagami');
+legend('Histogram', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma', ...
+                'Extreme Value', 'HalfNormal', 'Nakagami');
 hold off;
 
 F(9,:) = cdf(pd, y);
@@ -292,9 +300,9 @@ F(9,:) = cdf(pd, y);
 figure(2)
 hold on;
 plot(y, F(9,:), 'r');
-legend('Empirical', 'Gumbel', 'Exponential', 'Lognormal', 'Normal', ...
-                'Weibull', 'Gamma', 'ExtremeValue', 'HalfNormal', ...
-                        'Nakagami');
+legend('Empirical', 'Generalized Extreme Value', 'Exponential', ...
+                'Lognormal', 'Normal', 'Weibull', 'Gamma', ...
+                'Extreme Value', 'HalfNormal', 'Nakagami');
 hold off;
 
 figure(3)
